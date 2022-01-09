7cf98b5ff32a3f4cba3afcdac1bd87583bfb37f7
João Vitor <71740709+Joao24820@users.noreply.github.com>
2022-01-09 19:27:56 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,5 @@
+num1 = float(input('Informe o primeiro numero escolhido: '))
+num2 = float(input('Informe o segundo numero escolhido: '))
+soma = (num1 + num2)
+
+print('A soma entre o numero {} e o numero {} é igual ao valor {} '.format(num1, num2, soma))
